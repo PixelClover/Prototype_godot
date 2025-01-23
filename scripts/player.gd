ff2d2a829b56defdd9e56c4c4ba6206123ecd812
PixelClover <pixelclover@yahoo.com>
2025-01-23 16:47:50 +0800

    Были отрефакторены и деббажены ошибки в коде.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,46 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0 				#Скорость передвижения
+const JUMP_VELOCITY = -400.0		# Высота прыжка
+const JUMP_BUFFER_TIME = 0.2		# Время буферизации прыжка в секундах
 
-# Переменные для обработки прыжка
-var jump_buffer_frames: int = 5 # Количество кадров для буферизации прыжка (Можешь поиграться с этим)
-var jump_buffer_counter: int = -1 # Счетчик для буферизации прыжка
-var ledge_assistance_frames: int = 6 # Количество кадров "милосердия"
-var ledge_assistance_counter: int = 0 # Счетчик для "помощи на краю" (также и с этим, чтобы понять что происходит)
+# Переменные
+var jump_buffered: bool = false		# включатель буферизации прыжка
+var jump_buffer_time: float = 0.0	# время буферизации прыжка
+
+var ledge_assistance_frames: int = 11 # Количество кадров "милосердия" 
+var ledge_assistance_counter: int = 0 # Счетчик для "помощи на краю"
+var jumped_from_ledge: bool = false  # Флаг для отслеживания прыжка с края
 
 func _physics_process(delta: float) -> void:
 	# Добавление гравитации
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		ledge_assistance_counter -= 1 # Уменьшаем счетчик "помощи на краю"
+		# Увеличиваем счетчик "помощи на краю", если не на земле
+		handle_ledge_assistance()
 
 	# Обработка прыжка
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
-			jump_buffer_counter = jump_buffer_frames
-		elif ledge_assistance_counter >= 0: # Если "помощь на краю" активна
+		# Реализицая "помощи у края платформы" в обработке прыжка
+		elif ledge_assistance_counter < ledge_assistance_frames and not jumped_from_ledge:
 			velocity.y = JUMP_VELOCITY
-			ledge_assistance_counter = 0 # Сбрасываем счетчик "помощи на краю"
+			ledge_assistance_counter = 0  # Сбрасываем счетчик "помощи на краю"
+			jumped_from_ledge = true
+		# Реализация "буферизации прыжка" в обработке прыжка
+		else:
+			jump_buffered = true
+			jump_buffer_time = Time.get_ticks_msec() / 1000.0
 
-	# Проверка буфера прыжка
-	if jump_buffer_counter >= 0 and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jump_buffer_counter -= 1
-
-	# Счетчик буфера
-	if jump_buffer_counter >= 0:
-		jump_buffer_counter -= 1
-
+	# Сброс счетчиков, если игрок на земле
+	if is_on_floor():
+		if jump_buffered and (Time.get_ticks_msec() / 1000.0 - jump_buffer_time < JUMP_BUFFER_TIME):
+			velocity.y = JUMP_VELOCITY
+			jump_buffered = false
+		ledge_assistance_counter = 0  # Сбрасываем "помощь на краю"
+		jumped_from_ledge = false #сбрасываем флаг 
+			
 	# Получение направления и обработка движения/замедления
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
@@ -40,8 +48,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Проверка на "помощь на краю"
-	if not is_on_floor() and ledge_assistance_counter < ledge_assistance_frames:
-		ledge_assistance_counter += 1 # Увеличиваем счетчик "помощи на краю"
-
 	move_and_slide()
+
+# Функция для обработки "помощи на краю"
+func handle_ledge_assistance() -> void:
+	if not is_on_floor() and ledge_assistance_counter < ledge_assistance_frames:
+		ledge_assistance_counter += 1
+			
